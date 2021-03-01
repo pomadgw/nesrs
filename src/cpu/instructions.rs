a60b53e8d88e8c5afe1e43eaa6949126de383436
Rahadian Yusuf <rahadian.y@gmail.com>
2021-03-01 23:21:42 +0700

    add comment & intentionally waste a read in inc

--- src/cpu/instructions.rs
+++ src/cpu/instructions.rs
@@ -16,6 +16,20 @@ macro_rules! set_instruction {
     }};
 }
 
+/// LDA (LoaD Accumulator)
+/// Affects Flags: N Z
+///
+/// MODE           SYNTAX       HEX LEN TIM
+/// Immediate     LDA #$44      $A9  2   2
+/// Zero Page     LDA $44       $A5  2   3
+/// Zero Page,X   LDA $44,X     $B5  2   4
+/// Absolute      LDA $4400     $AD  3   4
+/// Absolute,X    LDA $4400,X   $BD  3   4+
+/// Absolute,Y    LDA $4400,Y   $B9  3   4+
+/// Indirect,X    LDA ($44,X)   $A1  2   6
+/// Indirect,Y    LDA ($44),Y   $B1  2   5+
+///
+/// + add 1 cycle if page boundary crossed
 macro_rules! lda {
     ($self:expr, $memory:expr) => {
         $self.a = $memory.read($self.absolute_address, false);
@@ -23,11 +37,20 @@ macro_rules! lda {
     };
 }
 
+/// INC (INCrement memory)
+/// Affects Flags: N Z
+//
+/// MODE           SYNTAX       HEX LEN TIM
+/// Zero Page     INC $44       $E6  2   5
+/// Zero Page,X   INC $44,X     $F6  2   6
+/// Absolute      INC $4400     $EE  3   6
+/// Absolute,X    INC $4400,X   $FE  3   7
+
 // INC opcode invokes double read-write
 macro_rules! inc {
     ($self:expr, $memory:expr) => {
+        $memory.read($self.absolute_address, false);
         let mut temp = $memory.read($self.absolute_address, false);
-        temp = $memory.read($self.absolute_address, false);
 
         $memory.write($self.absolute_address, temp);
         temp = temp.wrapping_add(1);
