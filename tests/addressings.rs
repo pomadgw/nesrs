5add947de5b7696a3ee852df52070a80c20b091b
Rahadian Yusuf <rahadian.y@gmail.com>
2021-03-02 21:00:12 +0700

    fix: movetest for NZ set of LDA

--- tests/addressings.rs
+++ tests/addressings.rs
@@ -178,48 +178,6 @@ mod tests {
     }
 
     #[test]
-    fn lda_set_z() {
-        let mut bus = DummyBus::new();
-        let mut cpu = nesrs::cpu::CPU::new();
-        bus.ram[0x0000] = 0xad;
-        bus.ram[0x0001] = 0x00;
-        bus.ram[0x0002] = 0x01;
-        bus.ram[0x0100] = 0x00;
-
-        loop {
-            cpu.clock(&mut bus);
-
-            if cpu.is_clocking_done() {
-                break;
-            }
-        }
-
-        assert_eq!(cpu.a, 0x00);
-        assert_eq!(cpu.get_status(nesrs::cpu::CPUStatus::Z), true);
-    }
-
-    #[test]
-    fn lda_set_n() {
-        let mut bus = DummyBus::new();
-        let mut cpu = nesrs::cpu::CPU::new();
-        bus.ram[0x0000] = 0xad;
-        bus.ram[0x0001] = 0x00;
-        bus.ram[0x0002] = 0x01;
-        bus.ram[0x0100] = 0x81;
-
-        loop {
-            cpu.clock(&mut bus);
-
-            if cpu.is_clocking_done() {
-                break;
-            }
-        }
-
-        assert_eq!(cpu.a, 0x81);
-        assert_eq!(cpu.get_status(nesrs::cpu::CPUStatus::N), true);
-    }
-
-    #[test]
     fn lda_abx_no_cross_page() {
         let mut bus = DummyBus::new();
         let mut cpu = nesrs::cpu::CPU::new();
