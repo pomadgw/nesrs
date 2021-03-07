14234cdf3265720667fa45d07e16c4c83f8aa204
Rahadian Yusuf <rahadian.y@gmail.com>
2021-03-07 22:41:33 +0700

    fix: rename

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
 pub trait Memory {
-    fn read(&self, addressing: u16, is_read_only: bool) -> u8;
-    fn write(&mut self, addressing: u16, value: u8);
+    fn read(&self, address: u16, is_read_only: bool) -> u8;
+    fn write(&mut self, address: u16, value: u8);
 }
