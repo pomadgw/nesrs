d86495753bf601f6edd26de29c82875a358ae4da
Rahadian Yusuf <rahadian.y@gmail.com>
2021-05-12 05:18:15 +0700

    add registers

--- src/cpu.rs
+++ src/cpu.rs
@@ -3,3 +3,37 @@ mod test;
 pub fn hello() {
     println!("Hello");
 }
+
+pub struct CPURegisters {
+    pub a: u8,
+    pub x: u8,
+    pub y: u8,
+    pub sp: u8,
+    pub p: u8,
+    pub pc: u16,
+}
+
+impl Default for CPURegisters {
+    fn default() -> Self {
+        Self {
+            a: 0,
+            x: 0,
+            y: 0,
+            sp: 0,
+            p: 0,
+            pc: 0
+        }
+    }
+}
+
+pub struct CPU {
+    pub regs: CPURegisters
+}
+
+impl CPU {
+    pub fn new() -> Self {
+        CPU {
+            regs: Default::default(),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,7 @@ use nesrs;
 
 fn main() {
     nesrs::cpu::hello();
+    let cpu = nesrs::cpu::CPU::new();
+
+    println!("{}", cpu.regs.a);
 }
