7692e124f9bbc62fdd74197cad6b385b7b7bd4ed
Rahadian Yusuf <rahadian.y@gmail.com>
2021-06-21 16:33:57 +0700

    try the rom test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use nesrs;
 use nesrs::cpu::*;
 use nesrs::memory::*;
-use std::time::Instant;
+// use std::time::Instant;
 
 use std::fs::File;
 use std::io::prelude::*;
@@ -26,28 +26,30 @@ impl Memory for RAM {
 fn main() -> std::io::Result<()> {
     let mut cpu = CPU::new();
 
-    let mut file = File::open("./test.rom")?;
+    let mut file = File::open("./6502_test.rom")?;
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer)?;
 
     let mut memory = RAM { a: buffer };
 
     cpu.debug = true;
-    cpu.reset();
-    loop_cpu!(cpu, memory);
+    // cpu.reset();
+    // loop_cpu!(cpu, memory);
+
+    cpu.regs.pc = 0x0400;
     println!(
         "${:04X}: A: ${:02X} X: ${:02X} Y: ${:02X}",
         cpu.regs.pc, cpu.regs.a, cpu.regs.x, cpu.regs.y
     );
 
-    let now = Instant::now();
+    // let now = Instant::now();
 
     for _i in 0..10 {
-        let prev = now.elapsed().as_nanos();
+        // let prev = now.elapsed().as_nanos();
         loop_cpu!(cpu, memory);
         cpu.print_debug();
-        let current = now.elapsed().as_nanos();
-        println!("time: {} ns", current - prev);
+        // let current = now.elapsed().as_nanos();
+        // println!("time: {} ns", current - prev);
     }
 
     println!(
