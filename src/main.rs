903c20a3c7e9843e2a46f46b450bfdf3f0c9bace
Rahadian Yusuf <rahadian.y@gmail.com>
2021-06-22 19:07:18 +0700

    measure

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::io::prelude::*;
 use std::io::SeekFrom;
 use std::mem;
 use std::slice;
+use std::time::Instant;
 
 #[macro_use]
 mod macros;
@@ -92,14 +93,32 @@ fn main() -> std::io::Result<()> {
         ram: vec![0; 0x8000],
     };
 
-    cpu.debug = true;
+    cpu.debug = false;
     cpu.reset();
     loop_cpu!(cpu, memory);
     cpu.regs.pc = 0xc000;
 
+    let now = Instant::now();
+    let start = now.elapsed().as_micros();
+
     for _i in 0..8991 {
         loop_cpu!(cpu, memory);
         cpu.print_debug();
+    }
+
+    let end = now.elapsed().as_micros();
+    let dur = end - start;
+    let dur_per_cycle = dur as f32 / cpu.total_cycles as f32;
+    let freq = cpu.total_cycles as f32 / (dur as f32 / 1_000_000.0);
+
+    eprintln!("duration: {:}us, cycles: {}", dur, cpu.total_cycles);
+    eprintln!("duration / cycles: {} us/cycle", dur_per_cycle);
+    if freq < 1_000_000.0 {
+        eprintln!("freq: {} Hz", freq);
+    } else if freq > 1_000_000_000.0 {
+        eprintln!("freq: {} KHz", freq / 1_000.0);
+    } else {
+        eprintln!("freq: {} MHz", freq / 1_000_000.0);
     }
 
     Ok(())
