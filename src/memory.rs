39ca8be281d4ebf7700aacd38521df0cdb056be0
Rahadian Yusuf <rahadian.y@gmail.com>
2021-06-23 14:31:11 +0700

    modify memory trait

--- src/memory.rs
+++ src/memory.rs
@@ -1,4 +1,4 @@
 pub trait Memory {
-    fn read(&self, address: usize, is_read_only: bool) -> u8;
+    fn read(&mut self, address: usize, is_read_only: bool) -> u8;
     fn write(&mut self, address: usize, value: u8);
 }
