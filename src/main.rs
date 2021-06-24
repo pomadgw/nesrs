e925054aa9cab1e3ea3155236436c0492b6c850d
Rahadian Yusuf <rahadian.y@gmail.com>
2021-06-24 19:30:46 +0700

    create bus

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use nesrs;
 use nesrs::bus::*;
 use nesrs::cartridge::*;
-use nesrs::cpu::*;
 
 use std::fs::File;
 use std::io::prelude::*;
@@ -11,35 +10,35 @@ use std::time::Instant;
 mod macros;
 
 fn main() -> std::io::Result<()> {
-    let mut cpu = CPU::new();
-
     let mut file = File::open("./rom/nestest.nes")?;
     let mut buffer = Vec::new();
 
     file.read_to_end(&mut buffer)?;
+    let mut cartridge = Cartridge::parse(&buffer);
 
-    let mut memory = NesMemoryMapper::new(Cartridge::parse(&buffer));
+    let mut bus = Bus::new(&mut cartridge);
 
-    cpu.debug = true;
-    cpu.reset();
-    loop_cpu!(cpu, memory);
-
-    cpu.regs.pc = 0xc000;
+    bus.cpu.debug = false;
+    bus.reset();
+    bus.cpu.regs.pc = 0xc000;
 
     let now = Instant::now();
     let start = now.elapsed().as_micros();
 
-    for _i in 0..8991 {
-        loop_cpu!(cpu, memory);
-        cpu.print_debug();
+    for _i in 0..(8991 * 3) {
+        bus.clock();
+
+        if bus.cpu.done() {
+            bus.cpu.print_debug();
+        }
     }
 
     let end = now.elapsed().as_micros();
     let dur = end - start;
-    let dur_per_cycle = dur as f32 / cpu.total_cycles as f32;
-    let freq = cpu.total_cycles as f32 / (dur as f32 / 1_000_000.0);
+    let dur_per_cycle = dur as f32 / bus.cpu.total_cycles as f32;
+    let freq = bus.cpu.total_cycles as f32 / (dur as f32 / 1_000_000.0);
 
-    eprintln!("duration: {:}us, cycles: {}", dur, cpu.total_cycles);
+    eprintln!("duration: {:}us, cycles: {}", dur, bus.cpu.total_cycles);
     eprintln!("duration / cycles: {} us/cycle", dur_per_cycle);
     if freq < 1_000_000.0 {
         eprintln!("freq: {} Hz", freq);
