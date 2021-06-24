1ad9b4c44735afeb3106f838915a590970635093
Rahadian Yusuf <rahadian.y@gmail.com>
2021-06-24 19:30:46 +0700

    create bus

--- src/bus.rs
+++ src/bus.rs
@@ -1,13 +1,14 @@
 use crate::cartridge::*;
+use crate::cpu::*;
 use crate::memory::*;
 
-pub struct NesMemoryMapper {
+pub struct NesMemoryMapper<'a> {
     ram: Vec<u8>,
-    cartridge: Cartridge,
+    cartridge: &'a mut Cartridge,
 }
 
-impl NesMemoryMapper {
-    pub fn new(cartridge: Cartridge) -> NesMemoryMapper {
+impl<'a> NesMemoryMapper<'a> {
+    pub fn new(cartridge: &'a mut Cartridge) -> NesMemoryMapper {
         NesMemoryMapper {
             cartridge,
             ram: vec![0; 0x0800],
@@ -15,7 +16,7 @@ impl NesMemoryMapper {
     }
 }
 
-impl Memory for NesMemoryMapper {
+impl Memory for NesMemoryMapper<'_> {
     fn read(&mut self, address: usize, is_read_only: bool) -> u8 {
         let data = self.cartridge.read(address, is_read_only);
         if self.cartridge.use_cartridge_data() {
@@ -29,5 +30,39 @@ impl Memory for NesMemoryMapper {
         if address < 0x8000 {
             self.ram[address] = value;
         }
+    }
+}
+
+pub struct Bus<'a> {
+    memory_mapper: NesMemoryMapper<'a>,
+    pub cpu: CPU,
+    pub cycle: u32,
+}
+
+impl<'a> Bus<'a> {
+    pub fn new(cartridge: &'a mut Cartridge) -> Self {
+        Bus {
+            memory_mapper: NesMemoryMapper::new(cartridge),
+            cpu: CPU::new(),
+            cycle: 0,
+        }
+    }
+
+    pub fn clock(&mut self) {
+        match self.cycle {
+            0 | 3 => {
+                self.cpu.clock(&mut self.memory_mapper);
+            }
+            _ => {}
+        }
+        if self.cycle == 0 {
+            self.cycle = 6;
+        }
+
+        self.cycle -= 1;
+    }
+
+    pub fn reset(&mut self) {
+        self.cpu.reset();
     }
 }
