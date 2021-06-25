487c9d84967453a0a2527c3b19c4a0d0e9a9b20a
Rahadian Yusuf <rahadian.y@gmail.com>
2021-06-25 14:02:08 +0700

    add currently dummy rw for ppu

--- src/ppu.rs
+++ src/ppu.rs
@@ -1,5 +1,16 @@
 use crate::cartridge::CartridgeRef;
+use crate::memory::Memory;
 
 pub struct PPU {
     pub cartridge: CartridgeRef,
+}
+
+impl Memory for PPU {
+    fn read(&mut self, _address: usize, _is_read_only: bool) -> u8 {
+        0
+    }
+
+    fn write(&mut self, _address: usize, _value: u8) {
+        // ?
+    }
 }
