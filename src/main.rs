fc1c4ff1e6970851a16bb8e864790498d32a0379
Rahadian Yusuf <rahadian.y@gmail.com>
2021-06-28 08:47:24 +0700

    debug: render pattern to canvas

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@
 use nesrs;
 use nesrs::bus::*;
 use nesrs::cartridge::*;
+use nesrs::utils::*;
 
+use colored::*;
 use std::fs::File;
 use std::io::prelude::*;
 use std::time::Instant;
-use colored::*;
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
@@ -41,20 +42,20 @@ fn main() -> std::io::Result<()> {
     for _i in 0..(100000 * 3) {
         bus.clock();
 
-        // if bus.cpu.done() {
-        //     if debug_print_tick == 0 {
-        //         println!(
-        //             "{}",
-        //             bus.cpu
-        //                 .debug_with_other_info(&format!("PPU:{:3},{:3}", ppuscanline, ppucycle))
-        //         );
-        //         ppucycle = bus.ppu.borrow().cycle();
-        //         ppuscanline = bus.ppu.borrow().scanline();
-        //         debug_print_tick = 3;
-        //     }
+        if bus.cpu.done() {
+            if debug_print_tick == 0 {
+                eprintln!(
+                    "{}",
+                    bus.cpu
+                        .debug_with_other_info(&format!("PPU:{:3},{:3}", ppuscanline, ppucycle))
+                );
+                ppucycle = bus.ppu.borrow().cycle();
+                ppuscanline = bus.ppu.borrow().scanline();
+                debug_print_tick = 3;
+            }
 
-        //     debug_print_tick -= 1;
-        // }
+            debug_print_tick -= 1;
+        }
     }
 
     let end = now.elapsed().as_micros();
@@ -72,32 +73,46 @@ fn main() -> std::io::Result<()> {
         eprintln!("freq: {} MHz", freq / 1_000_000.0);
     }
 
-    for j in 0..8 {
-        for i in 0..16 {
-            let pattern_test = bus.ppu.borrow_mut().debug_pattern(0, i, j);
-            for y in 0..8 {
-                for x in 0..8 {
-                    let d = pattern_test[y * 8 + (7 - x)];
-                    let str = format!("█").truecolor(d.0, d.1, d.2);
-                    print!("{}", str);
-                }
-                println!("");
-            }
-            println!("\n-----------------\n");
-        }
+    match bus.ppu.borrow_mut().get_color(0, 0) {
+        (r, g, b) => println!("{}", "█".truecolor(r, g, b)),
     }
+
+    println!("");
+    for palette in 0..8 {
+        for pixel in 0..4 {
+            match bus.ppu.borrow_mut().get_color(palette, pixel) {
+                (r, g, b) => print!("{}", "█".truecolor(r, g, b)),
+            }
+        }
+        println!("");
+    }
+
+    // let ppuref = bus.ppu.borrow();
+
+    // let nametable = ppuref.debug_nametable(1);
+
+    // for y in 0..32 {
+    //     for x in 0..32 {
+
+    //         let index = y * 32 + x;
+    //         print!("{:02X}", nametable[index]);
+    //     }
+    //     println!("");
+    // }
 
     // for palette in 0..8 {
     //     for index in 0..4 {
     //         println!("{:?}", bus.ppu.borrow_mut().get_color(palette, index));
     //     }
     // }
-    /*
+
+    let mut debug_pattern: Screen = Screen::new(256, 128);
+
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
     let window = video_subsystem
-        .window("nesrs", 512, 480)
+        .window("nesrs", 512, 512)
         .position_centered()
         .build()
         .unwrap();
@@ -108,8 +123,21 @@ fn main() -> std::io::Result<()> {
     let mut texture = texture_creator
         .create_texture_streaming(
             PixelFormatEnum::RGBA32,
-            nesrs::ppu::NES_WIDTH_SIZE as u32,
-            nesrs::ppu::NES_HEIGHT_SIZE as u32,
+            256,
+            256,
+            // nesrs::ppu::NES_WIDTH_SIZE as u32,
+            // nesrs::ppu::NES_HEIGHT_SIZE as u32,
+        )
+        .map_err(|e| e.to_string())
+        .unwrap();
+
+    let mut texture_debug_pattern = texture_creator
+        .create_texture_streaming(
+            PixelFormatEnum::RGBA32,
+            (debug_pattern.width()) as u32,
+            (debug_pattern.height()) as u32,
+            // nesrs::ppu::NES_WIDTH_SIZE as u32,
+            // nesrs::ppu::NES_HEIGHT_SIZE as u32,
         )
         .map_err(|e| e.to_string())
         .unwrap();
@@ -132,14 +160,35 @@ fn main() -> std::io::Result<()> {
             }
         }
 
+        for patternindex in 0..2 {
+            for tile_y in 0..16 {
+                for tile_x in 0..16 {
+                    let pattern_test =
+                        bus.ppu
+                            .borrow_mut()
+                            .debug_pattern(patternindex, tile_x, tile_y);
+                    for row in 0..8 {
+                        let yy = tile_y * 8 + row;
+                        for col in 0..8 {
+                            let d = pattern_test[row * 8 + col];
+                            let xx =
+                                (tile_x * 8 + col) + (patternindex * debug_pattern.width() / 2);
+                            debug_pattern.set_pixel(xx, yy, d);
+                        }
+                    }
+                }
+            }
+        }
+
         bus.clock_until_frame_done();
 
         canvas.clear();
-        texture
+        texture_debug_pattern
             .update(
                 None,
-                bus.ppu.borrow().screen(),
-                nesrs::ppu::NES_WIDTH_SIZE * 3,
+                debug_pattern.image(),
+                debug_pattern.width() * 4,
+                // nesrs::ppu::NES_WIDTH_SIZE * 3,
             )
             .unwrap();
         canvas
@@ -148,15 +197,27 @@ fn main() -> std::io::Result<()> {
                 None,
                 Some(sdl2::rect::Rect::new(
                     0,
-                    0,
-                    nesrs::ppu::NES_WIDTH_SIZE as u32 * 2,
-                    nesrs::ppu::NES_HEIGHT_SIZE as u32 * 2,
+                    256,
+                    (debug_pattern.width() * 2) as u32,
+                    (debug_pattern.height() * 2) as u32,
                 )),
             )
             .unwrap();
+            canvas
+                .copy(
+                    &texture_debug_pattern,
+                    None,
+                    Some(sdl2::rect::Rect::new(
+                        0,
+                        256,
+                        (debug_pattern.width() * 2) as u32,
+                        (debug_pattern.height() * 2) as u32,
+                    )),
+                )
+                .unwrap();
 
         canvas.present();
     }
-    */
+
     Ok(())
 }
